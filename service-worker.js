f4042ae12187c0ae76d15f6f5ea90144dcfe1253
bellcookie <hye980804928@gmail.com>
2026-07-01 22:55:22 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+// 只缓存 App 外壳（HTML/CSS/JS/图标），不缓存基金数据本身，保证数据始终是最新请求
+const CACHE_NAME = "fund-tracker-shell-v1";
+const SHELL_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(SHELL_FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE_NAME; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event){
+  const url = event.request.url;
+  // 天天基金的数据请求永远走网络，不缓存，保证实时性
+  if(url.indexOf("fundgz.1234567.com.cn") !== -1){
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request);
+    })
+  );
+});
